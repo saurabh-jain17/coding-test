156c8288b3c2f8d86b0089c0fd8e14da47b66106
jainsau <jainsau@CACDPTZKZG.asiapacific.hpqcorp.net>
2018-09-12 13:02:34 +0530

    Commit >> list name in second loop

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -2,6 +2,7 @@ package com.monsanto.mbt;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Shipment {
@@ -29,13 +30,36 @@ public class Shipment {
 		List<Widget> widgetList1 = WidgetUtils.getSampleWidgets();
 		Collections.sort(widgetList1, new SortByDate());
 		count = 1 ;
-		for (Widget widget : widgetList) {
+		for (Widget widget : widgetList1) {
 			System.out.println(widget.getSerialNumber() + "\t" + widget.getColor() + "\t" + widget.getProductionDate());
 			count++;
 			if (count > 10) {
 				break;
 			}
 		}
+		
+		/*System.out.println("=================================================");
+		System.out.println("SN\tColor\tProductionDate");
+		
+		List<Widget> list = WidgetUtils.getSampleWidgets();
+		
+		Collections.sort(list, new Comparator<Widget>() {
+
+			public int compare(Widget o1, Widget o2) {
+				
+				return o1.getProductionDate().compareTo(o2.getProductionDate());
+			}
+		});
+		count = 1;
+		for (Widget widget : list) {
+			System.out.println(widget.getSerialNumber() + "\t" + widget.getColor() + "\t" + widget.getProductionDate());
+			count++;
+			if (count > 10) {
+				break;
+			}
+			
+		}*/
+		
 	}
 	
 }
